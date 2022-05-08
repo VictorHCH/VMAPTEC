15c7ebdcd795953bc59953b3f76aee6306c0d816
VictorHCH <82013957+VictorHCH@users.noreply.github.com>
2022-05-07 22:55:53 -0600

    Bonito

--- VMaptec/html/creditos.php
+++ VMaptec/html/creditos.php
@@ -9,24 +9,20 @@
 </head>
 
 <body>
-
-   
-
-
     <section id="main-content">
 
         <article>
             <header>
-            <?php
+                <?php
         require "../html/header.php";
     ?>
-    <div>
-                <h1>Guía de la Descripción de la Actividades Complementarias </h1>
-                <br>
+                <div>
+                    <h1>Guía de la Descripción de la Actividades Complementarias </h1>
+                    <br>
 
-                <br>
-                <h2>“Hoja membretada oficial”</h2>
-                <img src="../imagenes/Hoja_membretada.png">
+                    <br>
+                    <h2>“Hoja membretada oficial”</h2>
+                    <img src="../imagenes/Hoja_membretada.png">
             </header>
 
             <div class="content">
@@ -35,45 +31,56 @@
                 <p id="espacio"></p>
 
                 <h3>Tutoria:</h3>
-                <p>La tutoría es un programa que permite la mejora del proceso de aprendizaje por los estudiantes, permite apoyarlos en los ámbitos psicológicos, psicopedagógico, familiar social y económico.</p>
+                <p>La tutoría es un programa que permite la mejora del proceso de aprendizaje por los estudiantes,
+                    permite apoyarlos en los ámbitos psicológicos, psicopedagógico, familiar social y económico.</p>
                 <h4>Evidencias para la evaluación y acreditación</h4>
                 <p>Constancia de Acreditación expedida por el Departamento Académico correspondiente.</p>
                 <p id="espacio"></p>
 
                 <h3>Proyectos de Investigación:</h3>
-                <p>La investigación forma parte del proceso de formación integral ya que permite que los estudiantes desarrollen sus habilidades creativas, de invención y generación de ideas, así como, la expresión de las mismas en diversos foros.</p>
+                <p>La investigación forma parte del proceso de formación integral ya que permite que los estudiantes
+                    desarrollen sus habilidades creativas, de invención y generación de ideas, así como, la expresión de
+                    las mismas en diversos foros.</p>
                 <h4>Evidencias para la evaluación y acreditación</h4>
                 <p>Constancia de Acreditación expedida por el Departamento Académico correspondiente.</p>
                 <p id="espacio"></p>
 
                 <h3>Eventos académicos relacionados con la carrera:</h3>
-                <p>La participación en los diversos eventos académicos permite a los estudiantes compartir experiencias y autoevaluarse académicamente para enriquecer su preparación.</p>
+                <p>La participación en los diversos eventos académicos permite a los estudiantes compartir experiencias
+                    y autoevaluarse académicamente para enriquecer su preparación.</p>
                 <h4>Evidencias para la evaluación y acreditación</h4>
                 <p>Constancia de Acreditación expedida por el Departamento Académico correspondiente.</p><br>
                 <p id="espacio"></p>
 
                 <h3>Actividades extraescolares: </h3>
-                <p>Fomentar y promover la participación de los estudiantes en actividades de educación física, deporte, recreación, cultura y actividades cívicas para coadyuvar en el desarrollo integral del estudiante.</p>
+                <p>Fomentar y promover la participación de los estudiantes en actividades de educación física, deporte,
+                    recreación, cultura y actividades cívicas para coadyuvar en el desarrollo integral del estudiante.
+                </p>
                 <h4>Evidencias para la evaluación y acreditación</h4>
                 <p>Constancia de acreditación expedida por el Departamento de Actividades Extraescolares.</p>
                 <p id="espacio"></p>
 
                 <h3>Construcción de prototipos y desarrollo tecnológico:</h3>
-                <p>Al construir prototipos o realizar desarrollos tecnológicos, el estudiante aplica sus conocimientos y creatividad en la solución de problemas de carácter real y tangible.</p>
+                <p>Al construir prototipos o realizar desarrollos tecnológicos, el estudiante aplica sus conocimientos y
+                    creatividad en la solución de problemas de carácter real y tangible.</p>
                 <h4>Evidencias para la evaluación y acreditación</h4>
                 <p>Constancia de acreditación expedida por el Departamento Académico.</p>
                 <p id="espacio"></p>
 
                 <h3>Participación en ediciones:</h3>
-                <p>Al participar en ediciones el estudiante desarrolla las competencias de comunicación oral y escrita para potenciar el saber transferir sus aprendizajes, ideas, pensamientos e ideologías.</p>
+                <p>Al participar en ediciones el estudiante desarrolla las competencias de comunicación oral y escrita
+                    para potenciar el saber transferir sus aprendizajes, ideas, pensamientos e ideologías.</p>
                 <h4>Evidencias para la evaluación y acreditación</h4>
                 <p>Constancia emitida por el Departamento correspondiente.</p>
                 <p id="espacio"></p>
 
-                <h3>Programas de Apoyo a la Formación Profesional (emprendedurismo, productividad laboral, fomento a la lectura, conservación del medio ambiente, entre otros).</h3>
-                <p>La generación de valores y procesos que permitan fortalecer al formación profesional de la competencias del estudiante.</p>
+                <h3>Programas de Apoyo a la Formación Profesional (emprendedurismo, productividad laboral, fomento a la
+                    lectura, conservación del medio ambiente, entre otros).</h3>
+                <p>La generación de valores y procesos que permitan fortalecer al formación profesional de la
+                    competencias del estudiante.</p>
                 <h4>Evidencias para la evaluación y acreditación</h4>
-                <p>Constancia de acreditación expedida por el Departamento encargado de la operación del programa.</p><br><br>
+                <p>Constancia de acreditación expedida por el Departamento encargado de la operación del programa.</p>
+                <br><br>
 
             </div>
 
@@ -83,10 +90,7 @@
     </section>
     <!-- / #main-content -->
 
-
-
     <footer id="main-footer">
-
         <p><a href="../html/index.php">Regresar al inicio</a></p>
         <p>&copy; 2022-VMapTec</p>
     </footer>
